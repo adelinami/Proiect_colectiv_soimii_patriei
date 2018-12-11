52355b612885c0ddfa3c437cb1aa2a588f0efecb
ciprianmunteanu <justashadownfg@gmail.com>
2018-12-11 19:08:17 +0200

    idk

--- Frontend/client-app/src/components/provider/browse-jobs/BrowseJobs.tsx
+++ Frontend/client-app/src/components/provider/browse-jobs/BrowseJobs.tsx
@@ -8,6 +8,8 @@ import {createStyles, MuiThemeProvider, StyledComponentProps, Theme} from "@mate
 import Paper from "@material-ui/core/Paper/Paper";
 import TextField from "@material-ui/core/TextField/TextField";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import {FetchUtils} from "../../../utils/FetchUtils";
 
 interface Props extends StyledComponentProps{
   viewStore: ViewStore;
@@ -15,11 +17,17 @@ interface Props extends StyledComponentProps{
 
 interface State {
   type: string;
+  jobs: Job[];
 }
 
 const style = (themee: Theme) => createStyles({
   paper: {
-    margin: themee.spacing.unit * 5
+    margin: themee.spacing.unit * 5,
+  },
+  searchContainer: {
+    marginLeft: '10px',
+    alignSelf: 'left',
+    width: '30%'
   },
   input: {
     color: themee.palette.text.primary,
@@ -27,6 +35,10 @@ const style = (themee: Theme) => createStyles({
   },
   placeholder: {
     color: themee.palette.text.primary,
+  },
+  button: {
+    padding: '10px 12px',
+    marginTop: '20px'
   }
 });
 
@@ -36,24 +48,32 @@ class BrowseJobsBase extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {type:""};
+    this.state = {type:"", jobs: []};
   }
 
-  getJobs() {
-    return [
-      new Job("1", "cleaning", "clean my house", "bob321", "dinner", "20-09-2018"),
-      new Job("2", "dog walk", "walk good boie", "angry_cat98", "5", "23-09-2018"),
-      new Job("3", "kid", "take care of my toddler!!", "xfg45", "12", "30-09-2018"),
-    ];
+  componentDidMount(): void {
+    this.updateJobs();
+  }
+
+  updateJobs() {
+    const promise = FetchUtils.submitPostRequest("/search", {"type": this.state.jobs});
+
+    promise.then(json=>{
+      console.log(json);
+    }).catch(error=>{
+      console.log(error);
+    });
+
+    this.setState({jobs: [
+        new Job("1", "cleaning", "clean my house", "bob321", "dinner", "20-09-2018"),
+        new Job("2", "dog walk", "walk good boie", "angry_cat98", "5", "23-09-2018"),
+        new Job("3", "kid", "take care of my toddler!!", "xfg45", "12", "30-09-2018"),
+      ]});
   }
 
   handleSubmit = (event)=>{
     event.preventDefault();
-    console.log(this.state.type);
-    const data = {
-      username: this.state.type,
-    };
-    // handle based on data (update table)
+    this.updateJobs();
   };
 
   handleInputChange = (event)=>{
@@ -67,17 +87,22 @@ class BrowseJobsBase extends React.Component<Props, State> {
     return (
       <MuiThemeProvider theme = {theme}>
           <Paper className={classes.paper}>
-            <form onSubmit={this.handleSubmit}>
-              <TextField
-                label="Type"
-                className={classes.bootstrapInput}
-                InputLabelProps = {{style: {color: '#c7d2e2'}}}
-                value={this.state.type}
-                onChange={this.handleInputChange}
-                margin="normal"
-              />
-            </form>
-            <RecommendedJobsTable jobs={this.getJobs()}/>
+            <div className={classes.searchContainer}>
+              <form onSubmit={this.handleSubmit}>
+                <TextField
+                  label="Type"
+                  className={classes.bootstrapInput}
+                  InputLabelProps = {{style: {color: '#c7d2e2'}}}
+                  value={this.state.type}
+                  onChange={this.handleInputChange}
+                  margin="normal"
+                />
+                <Button variant="contained" color="primary" className={classes.button} type={"submit"}>
+                  Search
+                </Button>
+              </form>
+            </div>
+            <RecommendedJobsTable jobs={this.state.jobs}/>
           </Paper>
       </MuiThemeProvider>
     );
