8f959cb1780f2367412aaf2bf3741a5272fc4f3d
ciprianmunteanu <justashadownfg@gmail.com>
2018-12-11 11:12:44 +0200

    Forgot to merge #9

--- Frontend/client-app/src/components/provider/browse-jobs/BrowseJobs.tsx
+++ Frontend/client-app/src/components/provider/browse-jobs/BrowseJobs.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 import { inject, observer } from "mobx-react";
 import { ViewStore } from "src/store/view-store";
+import {RecommendedJobsTable} from "../recommended-jobs/RecommendedJobsTable";
+import {Job} from "../../../view-models/Job";
+import {theme} from "../../../themes/main-theme";
+import {MuiThemeProvider} from "@material-ui/core";
 
 interface Props {
-    viewStore: ViewStore;
+  viewStore: ViewStore;
 }
 
 @inject("viewStore")
 @observer
 export class BrowseJobs extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
+  constructor(props: Props) {
+    super(props);
+  }
+
+  getJobs() {
 
 
-    render(): React.ReactNode {
-        return <div>list of all jobs</div>;
-    }
+    return [
+      new Job("1", "cleaning", "clean my house", "bob321", "dinner", "20-09-2018"),
+      new Job("2", "dog walk", "walk good boie", "angry_cat98", "5", "23-09-2018"),
+      new Job("3", "kid", "take care of my toddler!!", "xfg45", "12", "30-09-2018"),
+    ];
+  }
+
+
+  render(): React.ReactNode {
+    return (
+      <MuiThemeProvider theme = {theme}>
+        <React.Fragment>
+          <RecommendedJobsTable jobs={this.getJobs()}/>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
 }
---
+++ Frontend/client-app/src/utils/FetchUtils.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class FetchUtils extends React.Component {
+  static submitPostRequest(path: string, params: string) {
+
+  }
+}
